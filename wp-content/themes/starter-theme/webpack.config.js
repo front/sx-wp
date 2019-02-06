56f7d19eb8b0499f0f059a4667ba60ab93a7600e
AJOTKA <agata.kowalewska@gmail.com>
2019-02-06 19:05:13 +0100

    svg files

--- wp-content/themes/starter-theme/webpack.config.js
+++ wp-content/themes/starter-theme/webpack.config.js
@@ -4,11 +4,10 @@ const extractCss = require('mini-css-extract-plugin');
 
 // const HTMLWebpackPlugin = require('html-webpack-plugin');
 // const glob = require('glob');
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
-const localConf = require('./local-config.json')
+const localConf = require('./local-config.json');
 
 // const generateHTMLPlugins = () =>
 //   glob.sync('./src/prototype/*.?(twig|html)').map(
@@ -22,124 +21,128 @@ const localConf = require('./local-config.json')
 const IS_PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  node: {
-    fs: 'empty',
-  },
-  // devServer: {
-  //   contentBase: path.join(__dirname, 'build/prototype'),
-  //   compress: false,
-  //   port: 8090,
-  //   writeToDisk: true,
-  //   open: 'Google Chrome'
-  // },
+    node: {
+        fs: 'empty',
+    },
+    // devServer: {
+    //   contentBase: path.join(__dirname, 'build/prototype'),
+    //   compress: false,
+    //   port: 8090,
+    //   writeToDisk: true,
+    //   open: 'Google Chrome'
+    // },
 
-  mode: IS_PROD ? 'production' : 'development',
+    mode: IS_PROD ? 'production' : 'development',
 
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'js/main.js',
-  },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/main.js',
+    },
 
-  externals: {
-      jquery: 'jQuery',
-      $: '$',
-  },
+    externals: {
+        jquery: 'jQuery',
+        $: '$',
+    },
 
-  resolve: {
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.jsx'],
-    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-  },
+    resolve: {
+        extensions: ['.web.js', '.mjs', '.js', '.json', '.jsx'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          extractCss.loader,
-          'css-loader',
-          'postcss-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              importer: magicImporter()
-            }
-          },
-        ],
-      },
-      // {
-      //   test: /\.(twig|html)$/,
-      //   use: [
-      //     'raw-loader',
-      //     {
-      //       loader: 'twig-html-loader',
-      //       options: {
-      //         data: dummyData,
-      //       },
-      //     },
-      //   ],
-      // },
-      {
-        test: /\.(gif|png|jpe?g)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: { disable: !IS_PROD },
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|svg)$/i,
-        include: path.resolve(__dirname, 'src/fonts'),
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8000,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
             },
-          },
+            {
+                test: /\.scss$/,
+                use: [
+                    extractCss.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            importer: magicImporter()
+                        }
+                    },
+                ],
+            },
+            // {
+            //   test: /\.(twig|html)$/,
+            //   use: [
+            //     'raw-loader',
+            //     {
+            //       loader: 'twig-html-loader',
+            //       options: {
+            //         data: dummyData,
+            //       },
+            //     },
+            //   ],
+            // },
+            {
+                test: /\.(gif|png|jpe?g)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            disable: !IS_PROD
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|svg)$/i,
+                include: path.resolve(__dirname, 'src/fonts'),
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8000,
+                        },
+                    }
+                ],
+            },
+            {
+                test: /.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                    },
+                    'svg-transform-loader',
+                    {
+                        loader: 'svgo-loader',
+                        options: {
+                            plugins: [
+                                { removeTitle: true },
+                                {
+                                    convertColors: {
+                                        shorthex: false,
+                                    },
+                                },
+                                { convertPathData: false },
+                            ],
+                        },
+                    },
+                ],
+            },
         ],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          { 
-            loader: 'svg-sprite-loader', 
-            options: { 
-	      extract: true,
-              spriteFilename: 'icons/icons.svg',
-              runtimeCompat: true
-	    } 
-          },
-          'svg-fill-loader',
-          'svgo-loader'
-        ]
-      },
-    ],
-  },
+    },
 
-  plugins: [
-    new extractCss({
-      filename: 'css/main.css',
-    }),
-    // ...generateHTMLPlugins(),
-    new BrowserSyncPlugin({
-      // BrowserSync options
-        host: 'localhost',
-        port: localConf.browserSync.port,
-        proxy: localConf.browserSync.proxy,
-        open: true
-    }),
-    new SpriteLoaderPlugin({
-        plainSprite: true,
-        spriteAttrs: {
-          id: 'my-custom-sprite-id'
-        }
-    })
-  ],
+    plugins: [
+        new extractCss({
+            filename: 'css/main.css',
+        }),
+        // ...generateHTMLPlugins(),
+        new BrowserSyncPlugin({
+            // BrowserSync options
+            host: 'localhost',
+            port: localConf.browserSync.port,
+            proxy: localConf.browserSync.proxy,
+            open: true,
+        }),
+    ],
 };
