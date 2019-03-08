b090ad9ae4f0eeb70f763c8728fe2b72f8c5f1e8
Łukasz Kajdan <lukasz.k@netkata.com>
2019-03-08 16:17:58 +0100

    finish article and archive view

--- wp-content/themes/starter-theme/single.php
+++ wp-content/themes/starter-theme/single.php
@@ -15,4 +15,32 @@ if ( ! class_exists( 'Timber' ) ) {
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 
+// get related posts
+$context['category'] = $context['posts'][0]->category;
+$post_id = $context['posts'][0]->ID;
+$category_id = $context['category']->ID;
+
+$args = array(
+	'posts_per_page'    => 4,
+	'offset'            => 0,
+	'cat'               => '',
+	'category_name'     => '',
+	'orderby'           => 'date',
+	'order'             => 'DESC',
+	'include'           => '',
+	'exclude'           => $post_id,
+	'meta_key'          => '',
+	'meta_value'        => '',
+	'post_type'         => 'post',
+	'post_mime_type'    => '',
+	'post_parent'       => '',
+	'author'	        => '',
+	'author_name'	    => '',
+	'post_status'       => 'publish',
+	'suppress_filters'  => true,
+	'fields'            => '',
+);
+$posts_in_category = get_posts( $args );
+$context['posts_in_category'] = get_posts( $args );
+
 Timber::render('single.twig', $context);
